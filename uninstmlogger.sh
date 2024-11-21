39cd12607f6af83eabb2a267826cd2f711c9f684
SoyToManco16 <5815957@alu.murciaeduca.es>
2024-11-22 00:32:01 +0100

    Update uninstmlogger.sh

--- uninstmlogger.sh
+++ uninstmlogger.sh
@@ -1,55 +1,42 @@
 #!/bin/bash
-# Este script debe ser ejecutado con permisos de root
 
+# Variables
 sysp="/etc/systemd/system"
-vlm="/var/log/mlog"
 ulbp="/usr/local/bin"
 culbp="$ulbp/mlogger.sh"
-servf="$sysp/mlogger.service"
+bsp="$ulbp/mloggerbackups.sh"  # Script de backup
+cjp="/etc/cron.d/mloggerbackups-cron"  # Cronjob
 
-# Detener el servicio si está en ejecución
-if systemctl is-active --quiet mlogger.service; then
-    echo "Deteniendo el servicio mlogger..."
-    systemctl stop mlogger.service
+# Eliminar el servicio
+echo "Deteniendo y deshabilitando el servicio mlogger..."
+systemctl stop mlogger.service
+systemctl disable mlogger.service
+rm -f "$sysp/mlogger.service"  # Eliminar el archivo del servicio
+
+# Recargar los servicios
+systemctl daemon-reload
+
+# Eliminar el cronjob si existe
+if [ -f "$cjp" ]; then
+    echo "Eliminando el cronjob de copias de seguridad..."
+    rm -f "$cjp"
+    systemctl restart cron  # Reiniciar el cron para aplicar los cambios
+else
+    echo "No se encontró el cronjob de copias de seguridad."
 fi
 
-# Deshabilitar el servicio para que no se inicie automáticamente
-if systemctl is-enabled --quiet mlogger.service; then
-    echo "Deshabilitando el servicio mlogger..."
-    systemctl disable mlogger.service
+# Eliminar el script de backups
+if [ -f "$bsp" ]; then
+    echo "Eliminando el script de copias de seguridad..."
+    rm -f "$bsp"
 fi
 
-# Eliminar el archivo de servicio
-if [ -f "$servf" ]; then
-    echo "Eliminando archivo de servicio en $servf..."
-    rm -f "$servf"
-fi
-
-# Eliminar el script de mlogger
+# Eliminar el script principal
 if [ -f "$culbp" ]; then
-    echo "Eliminando script de mlogger en $culbp..."
+    echo "Eliminando el script principal mlogger.sh..."
     rm -f "$culbp"
 fi
 
-# Renombrar mlog como antiguo si existe
-if [[ -f "$vlm" ]]; then
-    count=0
-    newmlog="${vlm}.old"
-
-    # Bucle para encontrar un nombre de archivo único
-    while [[ -f "${newmlog}" ]]; do
-        newmlog="${vlm}.old.${count}"
-        count=$((count + 1))
-    done
-
-    # Renombrar el archivo con el nuevo nombre único
-    mv "$vlm" "$newmlog"
-fi
-
-# Recargar los servicios para aplicar los cambios
-echo "Recargando lista de servicios..."
-systemctl daemon-reload
-
-# Mensaje de confirmación
-echo "Desinstalación de mlogger completada."
+# Finalizar
+echo "Desinstalación completada."
 clear
